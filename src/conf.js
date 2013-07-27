dbe8034967d4ca2f96498126ed60594c8d0b6843
advecchia <advecchia@inf.ufrgs.br>
2013-07-27 19:18:54 -0300

    sistema de configuração e teclas

---
+++ src/conf.js
@@ -0,0 +1,12 @@
+config = {
+		// Key binds
+		keys:{
+			left : me.input.KEY.LEFT,
+			right : me.input.KEY.RIGHT,
+			up : me.input.KEY.UP,
+			down : me.input.KEY.DOWN,
+			jump : me.input.KEY.X,
+			confirm : me.input.KEY.ENTER,
+			cancel : me.input.KEY.ENTER
+		}
+};
