9dec40f2fbecfd46f772b14ba03c4fce1a4d3e05
Hoarf <alan.ficagna@gmail.com>
2013-08-06 23:02:47 -0300

    mismerge fixed

--- src/resources.js
+++ src/resources.js
@@ -4,24 +4,13 @@ game.resources = [
      */
 
     // the main player spritesheet
-<<<<<<< HEAD
-    {name: "gripe_run_right",     type:"image",    src: "data/img/sprite/gripe_run_right.png"},
-    // the spinning coin spritesheet
-    {name: "spinning_coin_gold",  type:"image",    src: "data/img/sprite/spinning_coin_gold.png"},
-    // our enemty entity
-    {name: "wheelie_right",       type:"image",    src: "data/img/sprite/wheelie_right.png"},
-    // game font
-    {name: "font_white",          type:"image",    src: "data/img/font/font_white.gif"},
-=======
     // {name: "gripe_run_right",     type:"image",    src: "data/img/sprite/gripe_run_right.png"},
     // // the spinning coin spritesheet
     // {name: "spinning_coin_gold",  type:"image",    src: "data/img/sprite/spinning_coin_gold.png"},
     // // our enemty entity
     // {name: "wheelie_right",       type:"image",    src: "data/img/sprite/wheelie_right.png"},
-    // // game font
-    {name: "32x32_font",          type:"image",    src: "data/img/font/32x32_font.png"},
-
->>>>>>> 6d511a98bd3a7dad16fee1ed4de5718d33e642be
+    // game font
+    {name: "font_white",          type:"image",    src: "data/img/font/font_white.gif"},
     // title screen
     {name: "2_lados_title_screen",        type:"image",    src: "data/img/gui/2_lados_title.png"},
 
